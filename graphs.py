e61bddbca7d51185f1066649e123a958377da267
David Ecker <de846@nyu.edu>
2016-11-26 14:20:40 -0500

    Add methods for Node to have value, handled, and undo classmethods for Graph which are pointless

--- graphs.py
+++ graphs.py
@@ -5,20 +5,63 @@ class Node(object):
         self._value = value
         self._handled = False
 
+    @property
+    def value(self):
+        return self._value
+
+    @value.setter
+    def value(self, value):
+        print("Unable to change value of node once set")
+
+    @property
+    def handled(self):
+        return self._handled
+
+    @handled.setter
+    def handled(self, value):
+        if value == True:
+            self._handled = True
+        elif self._handled == True:
+            print("This node is node handled - can't unhandle a node.")
+            return
+
+    def __str__(self):
+        return str(self._value)
+
+    def __repr__(self):
+        return "Node({})".format(self._value)
+
 class Graph(object):
     def __init__(self, data):
-        self._edges = Graph.gen_adj_list(data)
+        self._nodes = self._add_nodes(data)
+        self._edges = self._gen_adj_list(data)
 
-    @classmethod
-    def gen_adj_list(cls, data):
+    def _add_nodes(self, data):
+        nodes = set()
+        for node_value in data:
+            nodes.add(Node(node_value))
+        return nodes
+
+    def _gen_adj_list(self, data):
         edges = []
         for edge in data:
             for edge_peer in data[edge]:
-                edges.append((edge, edge_peer))
+                for known_node in self._nodes:
+                    if edge_peer == known_node.value:
+                        edges.append((edge, edge_peer))
+                        continue
         return edges
 
+    @property
     def edges(self):
         return self._edges
+
+    @property
+    def nodes(self):
+        return self._nodes
+
+    # def __str__(self):
+        # return len(self)
 
 
 if __name__ == "__main__":
@@ -39,6 +82,15 @@ if __name__ == "__main__":
     }
 
     g = Graph(graph_data)
+    print(len(graph_data))
 
     # print the edges of our graph (adjaceny list)
-    print(g.edges())
+    print("edges of g:")
+    print(g.edges)
+
+    print("\nlooped nodes of g:")
+    gnodes = g.nodes
+    for gnode in gnodes:
+        print(gnode)
+    print("\nstr/repr of graph.nodes")
+    print(gnodes)
